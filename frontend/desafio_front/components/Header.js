8d4b3442764e46ce474a1a9213d1b67bb45afa78
cathe <cacateles12@gmail.com>
2023-01-09 17:42:36 -0300

    Feat: HandleClick function in header

--- frontend/desafio_front/components/Header.js
+++ frontend/desafio_front/components/Header.js
@@ -1,9 +1,18 @@
+import { useContext } from "react"
+import AppContext from "../context_api/AppContext";
 export default function Header() {
+  const {
+    clicked, setClicked
+  } = useContext(AppContext);
+
+  const handleClick = () => {
+    setClicked(!clicked);
+  }
   return (
   <header>
     <div className="header-container">     
       <img alt="logo" src="https://media.graphassets.com/XrnbNcaeSs65o67JT7Te?_gl=1*1t29x4w*_ga*NDUzNzQzNDgyLjE2NzE3MzUxNzY.*_ga_G6FYGSYGZ4*MTY3MTczNTE3OC4xLjEuMTY3MTczNTg5MS41OC4wLjA."/>
-      <h2>HOME</h2>
+      <button onClick={ handleClick }>HOME</button>
     </div>
   </header>)
 }
