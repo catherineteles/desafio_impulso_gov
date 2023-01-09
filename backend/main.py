75849ac667523be20301c5dbb03594d95111ded8
cathe <cacateles12@gmail.com>
2023-01-09 09:04:33 -0300

    Connecting db

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, Form
+from fastapi.middleware.cors import CORSMiddleware
+from databases import Database
 
 app = FastAPI()
 
+database = Database('sqlite+aiosqlite:///dados.db')
+
+
+@app.on_event("startup")
+async def database_connect():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def database_disconnect():
+    await database.disconnect()
+
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def fetch_data():
+    query = "SELECT * FROM estabelecimentos"
+    results = await database.fetch_all(query=query)
+
+    return  results
