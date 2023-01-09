a6c147f50ba09071a9399d21c15348f213a7510d
cathe <cacateles12@gmail.com>
2023-01-09 19:10:40 -0300

    Style: CSS for header and footer

--- frontend/desafio_front/components/Footer.js
+++ frontend/desafio_front/components/Footer.js
@@ -2,7 +2,11 @@ export default function Footer() {
   return (
   <footer>
     <div className="footer-container">     
-      <img alt="logo" src="https://media.graphassets.com/XrnbNcaeSs65o67JT7Te?_gl=1*1t29x4w*_ga*NDUzNzQzNDgyLjE2NzE3MzUxNzY.*_ga_G6FYGSYGZ4*MTY3MTczNTE3OC4xLjEuMTY3MTczNTg5MS41OC4wLjA."/>
+      <img
+        className="footer-img"
+        alt="logo"
+        src="https://media.graphassets.com/XrnbNcaeSs65o67JT7Te?_gl=1*1t29x4w*_ga*NDUzNzQzNDgyLjE2NzE3MzUxNzY.*_ga_G6FYGSYGZ4*MTY3MTczNTE3OC4xLjEuMTY3MTczNTg5MS41OC4wLjA."
+      />
     </div>
   </footer>)
 }
--- frontend/desafio_front/components/Header.js
+++ frontend/desafio_front/components/Header.js
@@ -11,8 +11,12 @@ export default function Header() {
   return (
   <header>
     <div className="header-container">     
-      <img alt="logo" src="https://media.graphassets.com/XrnbNcaeSs65o67JT7Te?_gl=1*1t29x4w*_ga*NDUzNzQzNDgyLjE2NzE3MzUxNzY.*_ga_G6FYGSYGZ4*MTY3MTczNTE3OC4xLjEuMTY3MTczNTg5MS41OC4wLjA."/>
-      <button onClick={ handleClick }>HOME</button>
+      <img
+        className="header-img"
+        alt="logo"
+        src="https://media.graphassets.com/XrnbNcaeSs65o67JT7Te?_gl=1*1t29x4w*_ga*NDUzNzQzNDgyLjE2NzE3MzUxNzY.*_ga_G6FYGSYGZ4*MTY3MTczNTE3OC4xLjEuMTY3MTczNTg5MS41OC4wLjA."
+      />
+      <button onClick={ handleClick } className="header-button">HOME</button>
     </div>
   </header>)
 }
