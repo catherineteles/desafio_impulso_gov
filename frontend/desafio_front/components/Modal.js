9a780229d7f76d7b2bdf431667be42573d8f26d9
cathe <cacateles12@gmail.com>
2023-01-09 18:30:56 -0300

    Feat: Functions handleChange and handleFilter in Modal component

--- frontend/desafio_front/components/Modal.js
+++ frontend/desafio_front/components/Modal.js
@@ -1,19 +1,36 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import AppContext from "../context_api/AppContext";
+import { data } from "../ListaMunicipios";
 
 export default function Modal() {
   const {
     clicked, setClicked
   } = useContext(AppContext);
 
+  const [searchInput, setSearchInput] = useState('');
+
   const handleClick = () => {
     setClicked(!clicked);
+  }
+
+  const handleChange = ({ target: { value } }) => {
+    setSearchInput(value)
+  }
+
+  const handleFilter = (city) => {
+    return city.nome.toLowerCase().includes(searchInput.toLowerCase())
   }
   return (
   <header>
     <div className="modal-container">     
       <button onClick={ handleClick }>X</button>
-      <input></input>
+      <div>
+        <p>Escolha um município</p>
+        <input onChange={ handleChange } />
+        {searchInput !== "" && data.filter(handleFilter).map((city) => 
+          <ul>{`${city.nome}, ${city.uf}`}</ul>
+        )}
+      </div>
     </div>
   </header>)
 }
